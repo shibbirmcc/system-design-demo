9e39504402e18d2dea8d363674f4b619dfe682a2
Shibbir Ahmed <shibbirmcc@gmail.com>
2022-04-04 21:11:09 +0200

    Project initialized

---
+++ ApplianceStatusStore/src/main/java/com/electrolux/demo/status/store/Application.java
@@ -0,0 +1,16 @@
+package com.electrolux.demo.status.store;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+	 Logger sLogger = LoggerFactory.getLogger(Application.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ ApplianceStatusStore/src/main/java/com/electrolux/demo/status/store/models/Appliance.java
@@ -0,0 +1,5 @@
+package com.electrolux.demo.status.store.models;
+
+public class Appliance {
+
+}
